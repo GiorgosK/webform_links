5ab0de5fdb81ac60bc8e183fee0008c8200c0a3d
Giorgos Kontopoulos <kongeo@gmail.com>
2019-04-04 01:16:28 +0300

    Initial commit

---
+++ src/Controller/DisplayWebformController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Drupal\webform_links\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\webform\Entity\Webform;
+use Drupal\Core\Link;
+
+/**
+ * Class DisplayWebformController.
+ */
+class DisplayWebformController extends ControllerBase {
+
+  /**
+   * List.
+   *
+   * @return string
+   *   Return Hello string.
+   */
+  public function list() {
+
+    $query = \Drupal::service('entity.query')->get('webform');
+    $entity_ids = $query->condition('status', 'open')->execute();
+    $webform_id = array();
+    foreach($entity_ids as $webid){
+      $webform_id[]=$webid;
+    }
+
+    $webforms = Webform::loadMultiple($webform_id);
+    $webform_urls = [];
+    foreach($webforms as $webform){
+      $webform_urls[] = Link::fromTextAndUrl($webform->get('title'), $webform->toUrl());
+    }
+
+    $content = [
+      '#theme' => 'item_list',
+      '#list_type' => 'ul',
+      '#items' => $webform_urls,
+      '#attributes' => ['class' => 'mylist'],
+      '#wrapper_attributes' => ['class' => 'container'],
+    ];
+
+    return $content;
+
+  }
+
+}
